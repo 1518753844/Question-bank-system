25bfdd73f40ee1fd2c265f4b07c3b3ad457bd8e5
happylife <1518753844@qq.com>
2020-11-01 10:18:55 +0800

    first commit

---
+++ questionbanksystem/src/main/java/com/example/questionbanksystem/QuestionbanksystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionbanksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionbanksystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionbanksystemApplication.class, args);
+    }
+
+}
---
+++ questionbanksystem/src/test/java/com/example/questionbanksystem/QuestionbanksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionbanksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionbanksystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
